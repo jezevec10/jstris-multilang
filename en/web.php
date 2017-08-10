ec1279fee49288972e55a45da511fdddc1763846
Pavel Tuma <jezevec10@gmail.com>
2017-08-10 17:54:14 +0200

    Added new language files

---
+++ en/web.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+    'leaderboard' => 'Leaderboard',
+    'tournament' => 'Tournament',
+    'login' => 'Login',
+    'register' => 'Register',
+    'myProfile' => 'My profile',
+    'myImprovement' => 'My improvement',
+    'settings' => 'Settings',
+    'logout' => 'Logout',
+    
+    /* Leaderboard */
+    'name' => 'Name',
+    'time' => 'Time',
+    'blocks' => 'Blocks',
+    'PPS' => 'PPS',
+    'finesse' => 'Finesse',
+    'date' => 'Date',
+    'replay' => 'Replay',
+    'place' => 'Place',
+    'nli' => 'Not logged in!',
+    'today' => 'Today',
+    'lastWeek' => 'Last week',
+    'lastMonth' => 'Last month',
+    'lastYear' => 'Last year',
+    'allTime' => 'All time',
+    'bestPlayers' => 'Best players',
+    'bestGames' => 'Best games',
+    'bestSubs' => 'Best subs',
+    'latestGames' => 'Latest games',
+    'leastBlocks' => 'The least blocks',
+    'myTimes' => 'My times',
+    
+    /* Replay */
+    'load' => 'Load',
+    'play' => 'Play',
+    'pause' => 'Pause',
+    'showSeg' => 'Show segments',
+    'fastestSeg' => 'Fastest segments',
+    'slowestSeg' => 'Slowest segments',
+    'speedChart' => 'Speed chart',
+    'moreStats' => 'More stats',
+    
+    /* PW change */
+    'pwChange' => 'Change password',
+    'newPw' => 'New Password',
+    'againPw' => 'Again',
+    'applyChange' => 'Apply change',
+    
+    /* Profile */
+    'ownProfile' => 'This is your public profile accessible at',
+    'bestTimes' => 'Best times',
+    'allTimes' => 'View all sprint times',
+    'userImpr' => 'View improvement stats',
+    'gameStats' => 'Game stats',
+    'latestGames' => 'Latest games',
+    'noGames' => 'This user has not played any games yet.',
+    'gamesPlayed' => 'Games',
+    'totalTime' => 'Total time',
+    'linesSent' => 'Lines sent',
+    'linesRec' => 'Lines received',
+    'placedBlocks' => 'Placed blocks',
+    'maxAPM' => 'Max. APM',
+    'maxREN' => 'Max. Combo',
+    'longestGame' => 'Longest game',
+    'totalB2B' => 'Total B2Bs',
+    'mostSent' => 'Most sent',
+    'hours' => 'hours',
+    'minutes' => 'minutes',
+    
+    /* Stats */
+    'improvementStats' => 'Improvement stats',
+    //keep ':name'
+    'improvementInfo' => 'The following graph shows line race time (Y axis) vs game date (X axis). Highlighted line shows improvements in :name\'s personal records. You can place your cursor over any point to show the game details.',
+    'date' => 'Date',
+    'result' => 'Result',
+    'submit' => 'Submit',
+    'qSettings' => 'Query settings',
+    'dispAll' => 'Display all games',
+    
+    /* About */
+    'aboutInfo' => 'Jstris is a simple online multiplayer block game built in Javascript, HTML5 and Python (server).',
+    'version' => 'Version',
+    'discussion' => 'Discussion',
+    'learnMore' => 'Learn more',
+    'contact' => 'Contact',
+    'serverCoverage' => 'Server cost coverage',
+    'supporters' => 'Project supporters',
+    
+    /* Other */
+    'notFound' => 'Not found.',
+    'invalidLink' => 'Requested link is invalid.',
+    'brb' => 'Be right back.',
+    'tryAgain' => 'Site is being updated, please try again in a few moments.',
+];
