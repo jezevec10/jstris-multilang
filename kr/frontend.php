d2fe13a8a060b6da662be8f3f58c66d8fdf92cd9
Ecdldaiiere <36485907+Ecdldaiiere@users.noreply.github.com>
2018-02-18 08:16:26 -0500

    Added Korean translation (#16)
    * Add files via upload
    * Create frontend.php
    * Create game.php
    * Create web.php

---
+++ kr/frontend.php
@@ -0,0 +1,92 @@
+<?php
+return [
+    //On screen captions
+    'ready' => '준비',
+    'go' => '시작!',
+    'notFocused' => '초점이 맞지 않은', //max 13 characters
+    'clickToFocus' => '클릭하여 초점 맞추기', //max 30 characters
+    'specMode' => '관전 모드',
+    'specModeInfo' => '이제 관전 모드로 전환되었습니다.',
+    'endSpec' => '/play를 입력하여 게임에 참가하십시오.', //keep '/play'
+    'typeHelp' => '/help를 입력하여 가능한 커맨드를 보십시오.', //keep '/help'
+    'st' => 'st', //max 2, in 1st, can stay untranslated
+    'nd' => 'nd', //max 2, in 2nd, can stay untranslated
+    'rd' => 'rd', //max 2, in 3rd, can stay untranslated
+    'th' => 'th', //max 2, in 4th, can stay untranslated
+    'notPlaying' => '플레이 중이 아님', //max 12
+    'waitNext' => '라운드가 끝날 때까지 기다려주세요.',
+    'waitNext2' => '다음 라운드를 기다려주세요.',
+    'pressStart' => '\'새 게임\'을 눌러 게임을 시작하십시오.', //'New game' should be same as in game.newGame
+    //Chat
+    'warning' => '경고',
+    'inactive1' => '관전모드입니다. 채팅으로 변경 가능합니다.',
+    'inactive2' => '어떠한 조작도 없을 시 관전모드가 활성화 됩니다.',
+    'nickFill' => '타이핑 입력대로 사용자이름이 바뀝니다.',
+    'setButton' => '셋팅', //max 4
+    'sendButton' => '누름', //max 4
+    'rNameReq' => '방 이름이 필요합니다!',
+    'newsUser' => '뉴스', //special username in chat
+    'serverUser' => '서버', //special username in chat
+    'noNamed' => '이름없음', //user without name (guest), max 7, can stay untranslated
+    'watching' => '관전자', //in 'Watching: user1,user2..', altern.=Spectators
+    'userJoined'=>'입장 하였습니다.', //in 'User1 joined the room.'
+    'joined' => '입장', //in title: 'User1 joined!'
+    'userCame' => '관전자로 입장하였습니다.',  //in 'User1 came to watch.'
+    'isSpectating' => '관전자로 입장하였습니다.', //in 'User1 is now spectating.'
+    'spectator' => '관전자',
+    'hasLeft' => '님이 나갔습니다.', //in 'Spectator User1 has left.'
+    'signedAs' => '로그인 되었습니다.',
+    'welcome' => '환영합니다,', //in 'Welcome User1!'
+    'welcomeIn' => '어서오세요.', //in 'Welcome in RoomName.'
+    'noSpectators' => '현재 관전자가 없습니다.',
+    'replayAvailable' => '다시보기 사용가능',
+    'oldVer' => '현재 구 버전을 사용중입니다., CTRL+F5 키를 입력하여 신 버전으로 이용하세요.',
+    'privateRoom' => '현재 혼자하기를 진행중입니다. 대기실로 이동 후 멀티로 이동해 보세요.',
+    'restartInfo' =>  '재시작은 F4 또는 환경설정에서 설정가능',
+    'joinLinkInfo' => '혼자하기입니다. 링크를 통하여 입장 가능한 방입니다.',
+    //Dynamic elements
+    'received' => '받은', //max 15
+    'finesse' => '피네스',   //max 15
+    //List of rooms
+    'roomName' => '방 이름',
+    'games' => '게임', //number of games played in a room
+    'players' => '선수', //number of players
+    //Results
+    'name' => '이름',
+    'wins' => '승',
+    'time' => '시간', //as game duration
+    'received' => '받은',
+    'sent' => '보낸',
+    'blocks' => '블록', //as number of blocks placed
+    'ren' => '콤보',
+    //Practice mode result
+    'gameTime' => '시간', //in Sprint time: xx.xx
+    'see' => '보기', //in 'See 40L leaderboard'
+    'leaderboard' => '리더 보드',
+    //Less common strings, can stay untranslated
+    'warning2' => '경고',
+    'connected' => '연결됨',
+    'client' => '클라이언트',
+    'notConnected' => '연결되지 않음',
+    'connLost' => '접속 실패',
+    'lastGame' => '마지막 게임:',
+    'roomSettings' => '방 설정:',
+    'attack' => '공격',
+    'combo' => '콤보',
+    'solid' => '고체',
+    'clear' => '클리어',
+    'mode' => '모드',
+    'garbage' => '삭제해야 할 줄',
+    'garbageDelay' => '삭제해야 할 줄 딜레이',
+    'messiness' => '깎기 어렵게',
+    'replay' => '다시보기',
+    'rep' => 'Rep', //max 3, short for Replay
+    'rec' => 'Rec.', //max 4, short for Received
+    'enterNullDAS' => '널포미노에서 사용하는 DAS 값',
+    'suggestedIs' => '원하는 DAS 값',
+    'applyConfirm' => '적용',
+    'invalidDAS' => 'DAS 값 변경 실패',
+    'settingsChanged' => '게임중 설정이 변경되어 재생할 수 없습니다.',
+    'segment' => '구간',
+    'duration' => '지속시간',
+];
---
+++ kr/web.php
@@ -0,0 +1,95 @@
+<?php
+return [
+    'leaderboard' => '리더 보드',
+    'tournament' => '토너먼트',
+    'login' => '로그인',
+    'register' => '가입',
+    'myProfile' => '내 프로필',
+    'myImprovement' => '나의 기록들',
+    'settings' => '설정',
+    'logout' => '로그 아웃',
+    
+    /* Leaderboard */
+    'name' => '이름',
+    'time' => '시각',
+    'blocks' => '블록',
+    'PPS' => '초당 사용하는 블럭 갯수',
+    'finesse' => '피네스',
+    'date' => '날짜',
+    'replay' => '다시보기',
+    'place' => '공간',
+    'nli' => '로그인 실패',
+    'today' => '오늘',
+    'lastWeek' => '지난주',
+    'lastMonth' => '지난 달',
+    'lastYear' => '작년',
+    'allTime' => '모든 기록들',
+    'bestPlayers' => '최고의 선수들',
+    'bestGames' => '최고의 게임',
+    'bestSubs' => '가장 빠른 시간',
+    'latestGames' => '이전 게임들',
+    'leastBlocks' => '적은 블럭수',
+    'myTimes' => '나의 기록들',
+    
+    /* Replay */
+    'load' => '불러오기',
+    'play' => '게임하기',
+    'pause' => '멈춤',
+    'showSeg' => '구분해서 보기',
+    'fastestSeg' => '가장 빠른 구간',
+    'slowestSeg' => '가장 느린 구간',
+    'speedChart' => '속도 차트',
+    'moreStats' => '스탯 더보기',
+    
+    /* PW change */
+    'pwChange' => '비밀번호 변경',
+    'newPw' => '새 비밀번호',
+    'againPw' => '새 비밀번호 재입력',
+    'applyChange' => '변경하기',
+    
+    /* Profile */
+    'ownProfile' => '공개 프로필',
+    'bestTimes' => '가장 좋은 시간',
+    'allTimes' => '모든 스프린트 기록 보기',
+    'userImpr' => '더 많은 게임 내용',
+    'gameStats' => '게임 내용',
+    'latestGames' => '지난 게임',
+    'noGames' => '사용자가 아직 게임을 하지 않았습니다.',
+    'gamesPlayed' => '게임 플레이',
+    'totalTime' => '플레이 시간',
+    'linesSent' => '보냄 ',
+    'linesRec' => '받음',
+    'placedBlocks' => '사용한 블럭 갯수',
+    'maxAPM' => '최대 공격 속도',
+    'maxREN' => '최대 콤보',
+    'longestGame' => '가장 긴 게임',
+    'totalB2B' => '모든 백투백',
+    'mostSent' => '가장 많이 보낸 공격',
+    'hours' => '시간',
+    'minutes' => '분',
+    
+    /* Stats */
+    'improvementStats' => '더 많은 내용',
+    //keep ':name'
+    'improvementInfo' => '라인레이스 시간 (Y 축) vs 게임 한 날짜 (X 축). 강조된 선은 :name\'s 또는 개인 기록을 표시합니다. 커서를 통해 임의대로 세부 정보를 확인 할 수 있습니다.',
+    'date' => '날짜',
+    'result' => '결과',
+    'submit' => '제출',
+    'qSettings' => '질문',
+    'dispAll' => '모든 게임 보기',
+    
+    /* About */
+    'aboutInfo' => 'Jstris는 자바스크립트를 이용한 쉽고 편한 블록 게임이며, 서버는 HTML5 와 Python을 사용합니다.',
+    'version' => '버전',
+    'discussion' => '토론',
+    'learnMore' => '더보기',
+    'contact' => '연락처',
+    'serverCoverage' => '서버 후원',
+    'supporters' => '후원자',
+    
+    /* Other */
+    'notFound' => '찾을 수 없음',
+    'invalidLink' => '존재하지 않는 링크입니다.',
+    'brb' => '곧 돌아오겠습니다.',
+    'tryAgain' => '현재 업데이트 중이니, 조금 있다 다시 시도해주시기 바랍니다.',
+];
