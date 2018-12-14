f585a900a2702432b0946db33690b3b65d37fd95
jezevec10 <jezevec10@gmail.com>
2018-12-14 18:50:02 +0100

    Added Danish language

---
+++ da/frontend.php
@@ -0,0 +1,94 @@
+<?php
+return [
+    //On screen captions
+    'ready' => 'KLAR',
+    'go' => 'BEGYND!',
+    'notFocused' => 'Ude af fokus', //max 13 characters
+    'clickToFocus' => 'Klik her for at fokusere', //max 30 characters
+    'specMode' => 'Tilskuer tilstand',
+    'specModeInfo' => 'Du er nu i tilskuer tilstand.',
+    'endSpec' => 'Skriv /play for at tilslutte til spillet', //keep '/play'
+    'typeHelp' => 'Skriv /help for at se tilgængelige kommandoer.', //keep '/help'
+    'st' => 'st', //max 2, in 1st, can stay untranslated
+    'nd' => 'nd', //max 2, in 2nd, can stay untranslated
+    'rd' => 'rd', //max 2, in 3rd, can stay untranslated
+    'th' => 'th', //max 2, in 4th, can stay untranslated
+    'notPlaying' => 'Spiller ikke', //max 12
+    'waitNext' => 'Vent til runden slutter',
+    'waitNext2' => 'Vent til næste runde',
+    'pressStart' => 'Tryk \'Nyt spil\' to start', //'New game' should be same as in game.newGame
+    'slowDown' => 'Sænk farten', //When a speed limit is exceeded
+    'speedLimitIs' => 'Hastighedsgrænsen er', //In a sentence: The speed limit is 2 PPS
+    //Chat
+    'warning' => 'Advarsel',
+    'inactive1' => 'Tilskuer tilstand vil blive aktiveret. Skriv i chatten for at afbryde.',
+    'inactive2' => 'Inaktivitet fundet, naeste inaktive spil vil tilskuer tilstand blive aktiveret.',
+    'nickFill' => 'Skriv dit brugernavn for at få brug af chatten!',
+    'setButton' => 'Sæt', //max 4
+    'sendButton' => 'Send', //max 4
+    'rNameReq' => 'Rum navnet skal være udfyldt!',
+    'newsUser' => 'Nyheder', //special username in chat
+    'serverUser' => 'Server', //special username in chat
+    'noNamed' => 'Unkendt', //user wihtout name (guest), max 7, can stay untranslated
+    'watching' => 'Tilskuer', //in 'Watching: user1,user2..', altern.=Spectators
+    'userJoined'=>'tilsluttede til rummet', //in 'User1 joined the room.'
+    'joined' => 'tilsluttede', //in title: 'User1 joined!'
+    'userCame' => 'kom for at se på',  //in 'User1 came to watch.'
+    'isSpectating' => 'ser nu som tilskuer', //in 'User1 is now spectating.'
+    'spectator' => 'Tilskuer',
+    'hasLeft' => 'har forladt', //in 'Spectator User1 has left.'
+    'signedAs' => 'Du er logget ind som',
+    'welcome' => 'Velkommen,', //in 'Welcome User1!'
+    'welcomeIn' => 'Velkommen til rummet', //in 'Welcome in RoomName.'
+    'noSpectators' => 'Ingen ser på lige nu.',
+    'replayAvailable' => 'Genafspilning er tilgængelig.',
+    'oldVer' => 'Du har en ældre version, brug CTRL+F5 for at opdatere til nyere!',
+    'privateRoom' => 'Det her er dit private rum hvor du ikke vil blive forstyrret, mens du spiller. Tjek lobbyen for at se offentlige rum.',
+    'restartInfo' =>  'Genstart på F4 eller sæt brugerdefineret tast.',
+    'joinLinkInfo' => 'Det her rum er privat. Den eneste måde folk kan tilslutte til rummet er med dette link:',
+    //Dynamic elements
+    'received' => 'Modtaget', //max 15
+    'finesse' => 'Finesse',   //max 15
+    //List of rooms
+    'roomName' => 'Rum navn',
+    'games' => 'Spil spillet', //number of games played in a room
+    'players' => 'Spillere', //number of players
+    //Results
+    'name' => 'Navn',
+    'wins' => 'Vundet',
+    'time' => 'Tid', //as game duration
+    'received' => 'Modtaget',
+    'sent' => 'Sendt',
+    'blocks' => 'Blokke', //as number of blocks placed
+    'ren' => 'REN',
+    //Practice mode result
+    'gameTime' => 'tid', //in Sprint time: xx.xx
+    'see' => 'Se', //in 'See 40L leaderboard'
+    'leaderboard' => 'resultattavle',
+    //Less common strings, can stay untranslated
+    'warning2' => 'ADVARSEL',
+    'connected' => 'Tilsluttet',
+    'client' => 'klient',
+    'notConnected' => 'IKKE TILSLUTTET',
+    'connLost' => 'Tilslutnings fejl!',
+    'lastGame' => 'Sidste spil:',
+    'roomSettings' => 'Rum indstillinger:',
+    'attack' => 'Angreb',
+    'combo' => 'Combo',
+    'solid' => 'Solid',
+    'clear' => 'Clear',
+    'mode' => 'Tilstand',
+    'garbage' => 'Garbage',
+    'garbageDelay' => 'GarbageForsinkelse',
+    'messiness' => 'Messiness',
+    'replay' => 'Genafspil',
+    'rep' => 'Gen', //max 3, short for Replay
+    'rec' => 'Modt', //max 4, short for Received
+    'enterNullDAS' => 'Skriv numerisk NullpoMino DAS værdi:',
+    'suggestedIs' => 'Foretrukne DAS er',
+    'applyConfirm' => 'Anvend?',
+    'invalidDAS' => 'DAS værdien er invalid og er ikke skiftet!',
+    'settingsChanged' => 'Dine indstillinger er ændret mens du spillede. Replay ugyldiggjort.',
+    'segment' => 'Segmenter',
+    'duration' => 'Varighed',
+];
