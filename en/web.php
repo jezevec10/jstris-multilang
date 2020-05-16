fede3f7d87b0bb584f395b1d87e663b9b39b70cf
jezevec10 <jezevec10@gmail.com>
2020-05-16 03:09:12 +0200

    Removed additional space in en/web

--- en/web.php
+++ en/web.php
@@ -173,7 +173,7 @@ return [
 
     /* Chat reports */
     'reportU' => 'Report user',
-    'reportDesc' => ' Here you can create a report if you think someone has abused the game chat.',
+    'reportDesc' => 'Here you can create a report if you think someone has abused the game chat.',
     'user' => 'User',
     'reason' => 'Reason',
     'rr0' => 'Spam or unwanted advertising',
