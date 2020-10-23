4c649eca8bc7edff05ecb10d5a6449cfcae780a8
ecto0310 <ecto0310@gmail.com>
2020-10-23 20:29:40 +0900

    Update ja translation

---
+++ ja/supporter.php
@@ -0,0 +1,71 @@
+<?php
+
+// Supporter rewards translations
+
+return [
+    'donate' => '寄付', //menu item, short
+    'descr' => 'Jstrisに寄付をする事でサポーター特典を有効化します',
+    'supportJstris' => 'Jstrisを応援する',
+    'supportDesc1' => 'もし，このゲームを応援してくれるなら，このプロジェクトへの寄付をして，サーバー費用とさらなる開発を応援してください.',
+    'warnDelay' => '入金が確認されるまで10分程度かかる場合があります．確認され次第，サポーター特典を使用することが出来ます．',
+
+    't1Full' => '1段階目のサポータ',
+    't2Full' => '2段階目のサポータ',
+    't1' => '1段階目',
+    't2' => '2段階目',
+
+    //description of supporter rewards
+    'nameIcon' => '名前のアイコン',
+    'nameIconDesc' => '名前の前に表示されるアイコンを10個のアイコン(国旗含む)から1つを選択できます',
+    'emotes' => 'チャットでの絵文字',
+    'emotesDesc' => '<code>:</code>とJstrisのチャットで入力すると, Openmojiで利用可能な2000個以上の絵文字が表示され，選択できます.',
+    'discord' => 'JstrisのDiscordでの役職',
+    'discordDesc' => 'JstrisのDiscordサーバーでオレンジ色の名前になります．(JstrisとDiscordのアカウントがリンクされてる場合のみ)',
+    'list' => 'サポートリストの名前',
+    'listDesc' => 'ゲーム内のサポーターリストに名前とプロフィールへのリンクが表示されます．これは設定で無効化できます．',
+
+    'customColor' => 'チャットでの自由な色',
+    'customColorDesc' => '色を選択するツールを使って名前に自由な色を使うことができます．',
+    'moreIcons' => '2000個以上の名前のアイコン',
+    'moreIconsDesc' => 'Openmojiで利用可能な2000個以上の絵文字を名前の前に表示できます．読み込む度にランダムに表示することも出来ます．',
+    'blockSkins' => '100個以上のスキン',
+    'blockSkinsDesc' => 'ゲーム内の設定で100個以上のスキンが利用可能になります．またこれは他のプレイヤーのリプレイにも表示されます．',
+    'changeName' => 'ユーザー名の変更',
+    'changeNameDesc' => 'ユーザー名の変更は1ヶ月に1回のみ',
+    'allFromT1' => '1段階目の全て',
+
+    'perMonth' => ':cost /月', //":cost" is replaced with number
+    'customAmount' => '自由な金額の寄付',
+    'customAmountDesc' => '自由な金額を寄付したい場合はここで行うことができます．1回のみの寄付の場合寄付額に応じた期間サポーター特典を使用することが出来ます．',
+    'oneTime' => '1回のみ', //max. 8chars
+    'monthly' => '毎月',  //max. 8chars
+    'other' => 'その他',  // meaning Other amount than suggested
+    'estimate' => '見積もりを表示',
+    'dAmount' => '寄付金額',
+    'sDuration' => 'サポーター期間',
+    'learnMore' => 'より詳しく', //show more information
+    'paymentsInfo' => ':service_name.&nbsp; を経由して処理され，アカウントのダッシュボードからいつでもキャンセル出来ます．',
+
+    'gift' => 'ギフト特典',
+    'giftDesc' => '他のJstrisアカウントの名前で寄付することが出来ます．右側のテキストボックスにアカウント名を入力し，確定を押してください．その後，ここから送信されたギフトは送信先アカウントに適用されます．',
+    'giftTo' => 'ギフトを送信するアカウント',
+    'set' => '確定', //confirm, ok, apply,...
+
+    'rewardsAvailable' => '寄付へお礼を利用できます．',
+    'rewardsList' => 'Jstrisへの寄付をありがとうございます!以下の特典を有効にすることが出来ます．',
+    'monthlyActive' => 'あなたは :type の定期的な寄付をしています',
+    'customActive' => 'あなたは :type の寄付を寄付額に応じた :days 日間の特典が有効になっています．',
+
+    //configuration
+    'gift' => '特典',
+    'ingameIcon' => 'ゲーム内アイコン',
+    'countryFlag' => '国旗',
+    'iconEmote' => 'ゲーム内アイコンのカスタム絵文字',
+    'iconEmoteDesc' => '入力すると検索を開始します．絵文字の一覧は :link をご覧ください．',
+    'randIcon' => 'ロードする度にランダムなアイコンを表示',
+    'chatColor' => 'ゲーム内での名前の色を自由に選択',
+    'supporterList' => 'サポートリストの名前',
+    'roleAdded' => 'リンクしているDiscordアカウント :name に追加されました．',
+    'addDiscord' => '有効化する前に<a href=":link">Jstrisのアカウント設定</a>でDiscordアカウントを追加します．',
+    'apply' => '適用',
+];
