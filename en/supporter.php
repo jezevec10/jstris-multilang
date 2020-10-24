5382060de5d4df73ffdb3c02056094b665e048d9
Allustrate <54383863+Allustrate@users.noreply.github.com>
2020-10-24 08:08:54 +0800

    supporter translation
    supporter translation

---
+++ en/supporter.php
@@ -0,0 +1,71 @@
+<?php
+
+// Supporter rewards translations
+
+return [
+    'donate' => 'Donate', //menu item, short
+    'descr' => 'Support Jstris by sending a donation and enable Supporter rewards',
+    'supportJstris' => 'Support Jstris',
+    'supportDesc1' => 'If you appreciate this game, please consider a donation to this project and help cover server expenses and further development.',
+    'warnDelay' => 'It may take up to 10 minutes until the transaction is credited to your Jstris account. Then you can configure your Supporter benefits here.',
+
+    't1Full' => 'Tier 1 Supporter',
+    't2Full' => 'Tier 2 Supporter',
+    't1' => 'Tier 1',
+    't2' => 'Tier 2',
+
+    //description of supporter rewards
+    'nameIcon' => 'Name icon',
+    'nameIconDesc' => 'Select one of 10 icons (incl. your country flag) that will show before your name.',
+    'emotes' => 'Emotes in chat',
+    'emotesDesc' => 'By typing <code>:</code> in jstris chat, emote selection will open with 2000+ available emotes from the Openmoji Library.',
+    'discord' => 'Role on Jstris discord',
+    'discordDesc' => 'Orange name on Jstris Discord server. Only applicable if you link Discord account to Jstris.',
+    'list' => 'Name on the Supporters list',
+    'listDesc' => 'A link to your in-game profile will be inluded in the list of supporters. Can be disabled in the settings.',
+
+    'customColor' => 'Custom color in chat',
+    'customColorDesc' => 'You can set any name color you want using a color picker tool.',
+    'moreIcons' => '2000+ name icons',
+    'moreIconsDesc' => 'You can set any of the 2000+ emoji icons from the Openmoji Library as the icon before your name in the game and on the website. You can also enable random icon selection each time you load the game.',
+    'blockSkins' => '100+ more block skins',
+    'blockSkinsDesc' => '100+ More block skins will be available in the in-game Appearance settings. This skin selection will be visible to other players and in your replays.',
+    'changeName' => 'Option to change username',
+    'changeNameDesc' => 'Option to change user name once a month.',
+    'allFromT1' => 'and everything from Tier 1',
+
+    'perMonth' => ':cost per month', //":cost" is replaced with number
+    'customAmount' => 'Custom amount',
+    'customAmountDesc' => 'If you prefer to donate a custom amount, you can configure it here. In case of a one-time donation, the supporter benefits are assigned to your account for a duration based on the amount.',
+    'oneTime' => 'One time', //max. 8chars
+    'monthly' => 'Monthly',  //max. 8chars
+    'other' => 'Other',  // meaning Other amount than suggested
+    'estimate' => 'Show estimate',
+    'dAmount' => 'Donation amount',
+    'sDuration' => 'Supporter duration',
+    'learnMore' => 'Learn more', //show more information
+    'paymentsInfo' => 'Processed via :service_name.&nbsp; Can be cancelled anytime on account dashboard.',
+
+    'gift' => 'Gift rewards',
+    'giftDesc' => 'Here you can donate in the name of any other Jstris account. Enter the account username in the text field on the right and press Set. After that, any donation you will send from this page will be assigned to the chosen account.',
+    'giftTo' => 'Gift to account',
+    'set' => 'Set', //confirm, ok, apply,...
+
+    'rewardsAvailable' => 'Donation rewards available',
+    'rewardsList' => 'Thanks for your donation to Jstris! You may enable the following rewards.',
+    'monthlyActive' => 'You currently have :type subscription active.',
+    'customActive' => 'You have :type rewards enabled for :days days based on the donation amount.',
+
+    //configuration
+    'gift' => 'Gift rewards',
+    'ingameIcon' => 'In-game icon',
+    'countryFlag' => 'Country flag',
+    'iconEmote' => 'Custom emote as in-game icon',
+    'iconEmoteDesc' => 'Start typing to search. Visit :link for full list of emote names.',
+    'randIcon' => 'Select a new icon randomly every time the game is loaded',
+    'chatColor' => 'Custom in-game name color',
+    'supporterList' => 'Username mentioned in the Supporters list',
+    'roleAdded' => 'Added to your connected Discord account :name.',
+    'addDiscord' => 'Add your Discord account in the <a href=":link">Jstris account settings</a> before enabling.',
+    'apply' => 'Apply changes',
+];
