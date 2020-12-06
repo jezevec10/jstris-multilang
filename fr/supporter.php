a0c372628008067e65503df4567c1f2c3bf7345c
dnastudiosdavid <david@dna-studios.ch>
2020-12-06 11:17:58 +0100

    Translated Supporter section into French

---
+++ fr/supporter.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+    'donate' => 'Faire un don',
+    'descr' => 'Soutenez Jstris en faisant un don et gagnez des récompenses exclusives.',
+    'supportJstris' => 'Soutenez Jstris',
+    'supportDesc1' => 'Si vous appréciez ce jeu, pensez à faire un don pour soutenir ce projet. Vous aiderez ainsi à couvrir les dépenses liées aux serveurs et aux développements futurs.',
+    'warnDelay' => 'Cela peut prendre jusqu\'à 10 minutes avant que la transaction ne soit validée. Vous pourrez ensuite accéder à vos récompenses ici.',
+
+    't1Full' => 'Soutien Tier 1',
+    't2Full' => 'Soutien Tier 2',
+    't1' => 'Tier 1',
+    't2' => 'Tier 2',
+
+    //description of supporter rewards
+    'nameIcon' => 'Icône de pseudo',
+    'nameIconDesc' => 'Choisissez un des 10 icônes (y.c. le drapeau de votre pays) qui sera affiché devant votre pseudo.',
+    'emotes' => 'Emoticônes dans le tchat',
+    'emotesDesc' => 'En écrivant <code>:</code> dans le tchat de Jstris, vous aurez accès à plus de 2000 émoticônes provenant de la librairie Openmoji',
+    'discord' => 'Rôle sur le Discord Jstris',
+    'discordDesc' => 'Votre pseudo apparaît en orange sur le serveur Discord de Jstris. Disponible si vous liez votre compte Discord sur Jstris.',
+    'list' => 'Votre pseudo dans la liste des donateurs',
+    'listDesc' => 'Un lien vers votre profil sera créé depuis la liste des donateurs. Peut être désactivé dans les options.',
+
+    'customColor' => 'Couleur personnalisée dans le tchat',
+    'customColorDesc' => 'Vous pouvez choisir n\'importe quelle couleur pour votre pseudo.',
+    'moreIcons' => '2000+ icônes de pseudo',
+    'moreIconsDesc' => 'Choisissez parmis plus de 2000 icônes emoji provenant de la librairie Openmoji comme icône devant votre pseudo dans le jeu et sur le site. Possibilité d\'avoir un icône aléatoire à chaque fois que vous lancez le jeu.',
+    'blockSkins' => 'Plus de 100 skins supplémentaires de blocs',
+    'blockSkinsDesc' => 'Plus de 100 skins supplémentaires pour vos blocs seront disponibles dans les options du jeu. Le skin choisi sera visible par les autres joueurs et dans vos replays.',
+    'changeName' => 'Changement de pseudo',
+    'changeNameDesc' => 'Possibilité de changer votre pseudo une fois par mois.',
+    'allFromT1' => 'et tous les avantages du Tier 1',
+
+    'perMonth' => ':cost par mois', //":cost" is replaced with number
+    'customAmount' => 'Montant libre',
+    'customAmountDesc' => 'Si vous préférez donner un montant libre, vous pouvez le faire ici. Dans le cas d\'un versement unique, vos récompenses seront activées sur votre compte pour une durée correspondante au montant.',
+    'oneTime' => 'Unique', //max. 8chars
+    'monthly' => 'Mensuel', //max. 8chars
+    'other' => 'Autre', // meaning Other amount than suggested
+    'estimate' => 'Montrer l\'estimation',
+    'dAmount' => 'Montant du don',
+    'sDuration' => 'Durée des récompenses',
+    'learnMore' => 'En savoir plus', //show more information
+    'paymentsInfo' => 'Paiement opéré par :service_name.&nbsp; Peut être annulé à tout moment dans le tableau de bord du compte.',
+
+    'gift' => 'Offrir des récompenses',
+    'giftDesc' => 'Vous pouvez offrir les récompenses à autre compte Jstris en faisant un don en son nom. Entrez le pseudo du compte dans le champ de texte sur la droite et cliquez sur le bouton OK. Faites ensuite un don ci-dessus et les récompenses seront assignées au compte choisi.',
+    'giftTo' => 'Offrir au compte',
+    'set' => 'OK', //confirm, ok, apply,...
+
+    'rewardsAvailable' => 'Récompenses disponibles',
+    'rewardsList' => 'Merci pour votre don à Jstris! Vous avez obtenu les récpompenses suivantes.',
+    'monthlyActive' => 'Vous avez actuellement la souscription active suivante: :type',
+    'customActive' => 'Vous avez les récompenses :type activées pour :days jours, basé sur le montant du don effectué.',
+
+    //configuration
+    'ingameIcon' => 'Icône en jeu',
+    'countryFlag' => 'Drapeau du pays',
+    'iconEmote' => 'Emoticône personnalisé comme icône en jeu',
+    'iconEmoteDesc' => 'Ecrivez quelque chose pour rechercher. Visitez :link pour la liste complète des émoticônes.',
+    'randIcon' => 'Nouvel icône aléatoire à chaque fois que le jeu est lancé',
+    'chatColor' => 'Couleur personnalisée en jeu',
+    'supporterList' => 'Pseudo mentionné dans la liste des donateurs',
+    'roleAdded' => 'Ajouté à votre compte Discord lié :name.',
+    'addDiscord' => 'Ajoutez votre compte Discord dans les <a href=":link">paramètres du compte Jstris</a> avant d\'activer cette option.',
+    'apply' => 'Appliquer',
+];
