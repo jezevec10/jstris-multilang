610648909863604c7ed0b5f95755d4e967cea84e
Silgrond <58670531+Silgrond@users.noreply.github.com>
2021-12-31 14:39:41 +0100

    Hungarian translation added (#115)

---
+++ hu/supporter.php
@@ -0,0 +1,71 @@
+<?php
+
+// Supporter rewards translations
+
+return [
+    'donate' => 'Adomány', //menu item, short
+    'descr' => 'Támogasd a Jstris-t adományozással, hogy elérhetővé tedd a támogatói jutalmakat.',
+    'supportJstris' => 'Jstris támogatása',
+    'supportDesc1' => 'Ha kedveled a játékot, kérlek támogasd munkánkat adományozással, fedezve a szerverkiadásainkat és a jövőbeni fejlesztéseinket.',
+    'warnDelay' => 'Akár 10 percig is eltarthat, amíg a tranzakciót jóváírják a Jstris fiókodon. Ezután itt tudod beállítani a Támogatói juttatásokat.',
+
+    't1Full' => '1-es szintű Támogató',
+    't2Full' => '2-es szintű Támogató',
+    't1' => '1-es szint',
+    't2' => '2-es szint',
+
+    //description of supporter rewards
+    'nameIcon' => 'Név ikon',
+    'nameIconDesc' => 'Válassz egyet a 10 ikon közül (beleértve az országod zászlaját), amely a neved előtt jelenik meg.',
+    'emotes' => 'Emoteok a chatben',
+    'emotesDesc' => 'Beírva a <code>:</code> szót a jstris chatbe megnyílik az emote-kiválasztás, amelyen keresztül több mint 2000 elérhető érzelmet választhatsz ki az Openmoji könyvtárból.',
+    'discord' => 'Jstris Discord rang',
+    'discordDesc' => 'Narancssárga név a Jstris Discord szerveren. Csak akkor alkalmazható, ha a Discord-fiókod összekapcsolod a Jstris-szel.',
+    'list' => 'Név a Támogatók listáján',
+    'listDesc' => 'A Támogatók listáján a játékbeli profilodra mutató link fog szerepelni. A beállításokban kikapcsolható.',
+
+    'customColor' => 'Egyéni szín a chaten',
+    'customColorDesc' => 'Bármilyen színt beállíthatsz a nevedre a színválasztó eszközzel.',
+    'moreIcons' => 'Több mint 2000 név ikon',
+    'moreIconsDesc' => 'Az Openmoji könyvtárból származó több mint 2000 emoji ikon bármelyikét beállíthatod ikonként a neved előtt a játékban és a webhelyen. Engedélyezheted a játék betöltésekor az ikonok véletlenszerű kiválasztását is.',
+    'blockSkins' => 'Több mint 100 elem kinézet',
+    'blockSkinsDesc' => 'Több mint 100 elem kinézet elérhető a játékon belüli megjelenési beállításokban. A választott kinézet látható lesz a többi játékos számára és a visszajátszásokban.',
+    'changeName' => 'Felhasználónév váltás',
+    'changeNameDesc' => 'Lehetőség, hogy havonta egyszer megváltoztasd a felhasználónevedet.',
+    'allFromT1' => 'És minden az 1-es támgoatói szintről',
+
+    'perMonth' => ':cost havonta', //":cost" is replaced with number
+    'customAmount' => 'Egyéni összeg',
+    'customAmountDesc' => 'Ha inkább egy egyéni összeget szeretnél adományozni, itt tudod beállítani. Egyszeri adományozás esetén a támogatói előnyök az összeg alapján meghatározott időtartamra kerülnek hozzárendelésre a fiókodhoz.',
+    'oneTime' => 'Egyszeri', //max. 8chars
+    'monthly' => 'Havi',  //max. 8chars
+    'other' => 'Egyéni',  // meaning Other amount than suggested
+    'estimate' => 'Becslés megjelenítése',
+    'dAmount' => 'Adomány összege',
+    'sDuration' => 'Támogatás időtartama',
+    'learnMore' => 'További információ', //show more information
+    'paymentsInfo' => ':service_name -on keresztül történik.&nbsp; Bármikor lemondható a fiók beállításainál.',
+
+    'gift' => 'Jutalmak ajándékozása',
+    'giftDesc' => 'Itt bármely más Jstris-fiók nevében adományozhat. Írja be a fiók felhasználónevét a jobb oldali szövegmezőbe, majd nyomja meg a Beállítás gombot. Ezt követően az ezen az oldalon küldött adományok a kiválasztott fiókhoz kerülnek hozzárendelésre.',
+    'giftTo' => 'Megajándékozni kívánt felhasználó',
+    'set' => 'Beállítás', //confirm, ok, apply,...
+
+    'rewardsAvailable' => 'Adományozási jutalmak elérhetőek',
+    'rewardsList' => 'Köszönjük a Jstrisnek tett adományát! A következő jutalmakat engedélyezheti.',
+    'monthlyActive' => 'Jelenleg aktív :type előfizetésed van.',
+    'customActive' => 'Az adomány összege alapján az :type jutalmak :days napig elérhetőek.',
+
+    //configuration
+    'gift' => 'Jutalmak ajándékozása',
+    'ingameIcon' => 'Játékbeli ikon',
+    'countryFlag' => 'Országzászló',
+    'iconEmote' => 'Egyéni emote mint játékbeli ikon',
+    'iconEmoteDesc' => 'Kezdj el gépelni a kereséshez. Az emote nevek teljes listájáért látogasson el a :linkre.',
+    'randIcon' => 'Véletlenszerűen új ikont választ ki minden alkalommal, amikor a játék betöltődik',
+    'chatColor' => 'Egyéni játékbeli szín',
+    'supporterList' => 'A Támogatók listáján feltűntetett név',
+    'roleAdded' => 'Hozzáadva a :name néven csatlakoztatott Discord-fiókhoz.',
+    'addDiscord' => 'Add hozzá a Discord-fiókod <a href=":link">a Jstris fiókbeállításokban</a> az engedélyezés előtt.',
+    'apply' => 'Változtatások alkalmazása',
+];
