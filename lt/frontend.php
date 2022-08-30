3a7bcf253e271ca3b73bcd2c47aea63eb52defa3
m1zukash1 <75336116+m1zukash1@users.noreply.github.com>
2022-08-30 07:59:18 +0300

    I'm no professional translator by any means, but I doubt anyone else would do this.

---
+++ lt/frontend.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+    //On screen captions
+    'ready' => 'PASIRUOŠT!',
+    'go' => 'MARŠ!',
+    'waitNext' => 'Palaukite kol raundas pasibaigs',
+    'waitNext2' => 'Palaukite kol raundas pasibaigs',
+    'slowDown' => 'NE TAIP GREIT!', //When a speed limit is exceeded
+    'speedLimitIs' => 'Greičio limitas yra', //In a sentence: The speed limit is 2 PPS
+    'connecting' => 'Jungiamasi...', //when connecting to the game server
+    'signingIn' => 'Jungiamasi',
+    'loginFail' => 'Prisijungimas nepavyko!',
+    'loginFail2' => 'Jūs esate neprisijungęs! Jūs žaidžiate kaip svečias, vardu {name}.',
+
+    //List of rooms
+    'roomName' => 'Kambario pavadinimas',
+    'games' => 'Žaidimų', //number of games played in a room
+    'players' => 'Žaidėjų', //number of players
+
+    //Results
+    'name' => 'Vardas',
+    'wins' => 'Pergalių',
+    'time' => 'Laikas', //as game duration
+    'blocks' => 'Blokų padėta', //as number of blocks placed
+
+    //Practice mode result
+    'gameTime' => 'laikas', //in Sprint time: xx.xx
+    'see' => 'Žiūrėti', //in 'See 40L leaderboard'
+    'leaderboard' => 'lyderių lentelė',
+
+    //Room info detail
+    'noPlayers' => 'No players',
+    'cntMore' => '{cnt} more',          //e.g. +10 more
+    'cntGuests' => '{cnt} guests',      //e.g. +10 guests
+    'cntSpec' => '{cnt} spectating',    //e.g. +10 spectating
+    'joinPossible' => 'Join possible',
+    'notEligible' => 'Not eligible',
+    'gTimeShort' => 'G.time',
+    'on' => 'On',
+    'off' => 'Off',
+
+    //friends
+    'fr' => 'Draugai',
+    'frLoad' => 'Kraunamas draugų sąrašas.',
+    'frLogin' => 'Prisijungite, kad galėtumete naudotis draugų sąrašu.',
+    'frEmpty' => 'Draugų sąrašas yra tuščias.',
+    'frHowAdd' => 'Apsilankykite žaidėjo profilyje, kad galėtumete jį pakviesti į draugus.',
+    'frPriv' => 'Privatus',
+    'frIn' => 'Already in!',
+    'frChat' => 'Atidaryti chat\'ą',
+    'frRel' => 'Perkrauti',
+    'frMsgTo' => 'Parašyti {name}',  //keep {name}
+    'frInvBy' => 'nuo {user}',
+    'frPage' => 'Draugų siena',
+    'frIntroCl' => 'Uždaryti intro',
+];
