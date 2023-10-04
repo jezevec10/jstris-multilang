995ea4a240b7aa6b60f8e584f31638af1f9e37f3
Minju Kim <83636412+minjk-bl@users.noreply.github.com>
2023-10-04 22:56:30 +0900

    Edit frontend.php for Korean filling all langs

--- ko/frontend.php
+++ ko/frontend.php
@@ -36,7 +36,7 @@ return [
     'rNameReq' => '방 이름을 정해야 합니다!',
     'newsUser' => '소식', //special username in chat
     'serverUser' => '서버', //special username in chat
-    'noNamed' => '무명', //user without name (guest), max 7, can stay untranslated
+    'noNamed' => '이름없음', //user without name (guest), max 7, can stay untranslated
     'watching' => '관전 중', //in 'Watching: user1,user2..', altern.=Spectators
     'userJoined'=>'님이 들어왔습니다', //in 'User1 joined the room.' 게임에서 마침표를 붙이므로 여기선 붙일 필요가 없습니다.
     'joined' => '님 입장', //in title: 'User1 joined!'
@@ -106,7 +106,7 @@ return [
     'segment' => '구간',
     'duration' => '길이',
     'roomFull' => '방 인원이 가득 찼습니다.',
-    
+    'lobbyInfo' => '현재 채팅이 {discord}에 공유되었습니다.',
     'newHost' => '방장이 되었습니다.',
     'badRoom' => '방이 더이상 존재하지 않습니다, 대기실에 입장합니다!',
     'stngsChanged' => '방장에 의해 설정이 변경되었습니다.',
@@ -114,4 +114,77 @@ return [
     'aSpec' => '이미 관전 중입니다!',
     'aPlay' => '이미 플레이 중입니다!',
     'repFail' => '다시보기를 저장하지 못했습니다.'
+    'repInChat' => '다시보기 파일이 채팅에 출력되었습니다. 박스 안에 있는 문자를 복사하고 텍스트 에디터에 붙여넣어 저장하세요.',
+    'repTxtInfo' => '복사된 다시보기 파일은 사이트의 다시보기에서 재생하거나 재등록할 수 있습니다.',
+
+    'newPB' => '개인 신기록!',
+    'firstPB' => '이 게임은 첫 번째 게임입니다. 향상 통계를 확인하려면 개인 기록을 더 만드세요.',
+    'infoPB' => '이전 기록은 {prevPB}이며, {prevAgo}를 달성했습니다. 기록의 향상 차이는 {PBdiff}입니다.',
+    'daysAgo' => '일 전',
+
+    'raceFin' => '레이스 종료!',
+    'raceFinInfo' => '레이스를 완료할 수 있지만, 다음 라운드는 언제든지 시작할 수 있습니다.',
+
+    'notTSD' => 'NOT TSD',
+    'notTSDInfo' => 'T-Spin Double만으로 라인을 지우세요.',
+
+    'notPC' => 'NOT a PC',
+    'notPCInfo' => '매 10블록마다 Perfect Clear를 해야 합니다. 현재 보드는 클리어 할 수 없습니다.',
+
+    'fwDetect' => 'FOUR WIDE',
+    'fwDetectInfo' => '내 필드에 라인이 전송됩니다!',
+
+    'oops' => '앗!',
+    'chatNA' => '게스트나 {chReq}시간보다 적게 플레이한 사용자는 전체 채팅을 사용할 수 없습니다.',
+    'leMore' => '자세히 알아보기',
+
+    'connLimit' => '현재 이 IP에 대한 최대 오픈 연결량에 도달했습니다. 제한 범위를 늘리고 싶으시면 디스코드를 통해 연락주시길 바랍니다.',
+    'idleDC' => '자리비움으로 연결이 끊겼습니다! 관전 구역이 꽉 찼습니다.',
+    'RLreach' => '속도 제한에 도달했습니다.',
+    'ban1' => '라이브 게임에 대한 접근이 영구적으로 제한되었습니다. 여전히 싱글 플레이어 모드는 플레이할 수 있습니다.',
+    'ban2' => '이 사용자 계정은 정지되었습니다. 자세한 내용을 보려면 웹사이트에 다시 로그인하세요.',
+    'ncGS' => '게임 서버에 연결되지 않았습니다. {refr}을 해주세요.',
+    'refr' => '페이지 새로고침',
+
+    'nsUnpub' => '맵이 게시되지 않아, 기록이 저장되지 않습니다.',
+    'nsTspins' => 'T-Spins이 충분하지 않아, 기록이 저장되지 않습니다.',
+    'nsLowPC' => '최소 2개의 Perfect Clears가 필요합니다. 기록이 저장되지 않습니다.',
+    'noBlocks' => 'Out of blocks',
+    'noBlocks2' => '모든 블록을 사용했습니다.',
+
+    //Room info detail
+    'noPlayers' => '플레이어 없음',
+    'cntMore' => '{cnt}개 더',          //e.g. +10 more
+    'cntGuests' => '{cnt}명의 게스트',      //e.g. +10 guests
+    'cntSpec' => '{cnt}명이 관전 중',    //e.g. +10 spectating
+    'joinPossible' => '참여 가능',
+    'notEligible' => '참여할 수 없음',
+    'gTimeShort' => 'G.time',
+    'on' => 'On',
+    'off' => 'Off',
+
+    //friends
+    'fr' => '친구 목록',
+    'frLoad' => '친구 목록 여는 중',
+    'frLogin' => '친구 목록을 사용하려면 로그인하세요.',
+    'frEmpty' => '친구 목록이 비어 있습니다.',
+    'frHowAdd' => '사용자 프로필을 방문해서 친구 신청을 보내세요.',
+    'frPriv' => '비공개',
+    'frIn' => '이미 들어왔습니다!',
+    'frChat' => '채팅 열기',
+    'frRel' => '새로고침',
+    'frMsgTo' => '{name}에게 보내는 메세지',  //keep {name}
+    'frInv' => '방으로 초대하기',
+    'frInvTo' => '{room}으로 초대되었습니다.',
+    'frInvIn' => '이미 방에 있습니다!',
+    'frInvBy' => '{user} 보냄',
+    'frOn' => '온라인',
+    'frOff' => '오프라인',
+    'frNewChatH' => '{name}와의 채팅방이 시작되었습니다.',
+    'frWelc' => '친구 탭에 오신 것을 환영합니다',
+    'frIntro' => '이 곳에서는 온라인 친구 목록, 개인 채팅, 방 초대 등에 접속할 수 있습니다',
+    'frIntro2' => '친구 요청을 보내시려면 사용자의 프로필을 방문하세요',
+    'frIntro3' => '친구를 관리하려면 {frPage}를 방문하세요',
+    'frPage' => '친구 목록 페이지',
+    'frIntroCl' => '안내페이지 닫기',
 ];
