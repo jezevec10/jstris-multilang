d96bce53dd2f579e29704f339da4e9a5e4a66721
n0kisa00 <n0kisa00@students.oamk.fi>
2024-02-08 21:44:52 +0200

    Create and translate supporter.php in Finnish

---
+++ fi/supporter.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+    'donate' => 'Lahjoita', //menu item, short
+    'supportUs' => 'Tue meitä', //menu item, short
+    'descr' => 'Tue Jstristä ja ota Tukijan palkinnot käyttöön',
+    'supportJstris' => 'Tue Jstristä',
+    'supportDesc1' => 'Jos arvostat tätä peliä, harkitse ryhtymistä tämän projektin tukijaksi ja auta kattamaan sekä palvelinkulut että jatkokehitys.',
+    'warnDelay' => 'Voi kestää jopa 10 minuuttia, ennen kuin tapahtuma hyvitetään Jstris-tilillesi. Sitten voit määrittää tukijaetusi täällä.',
+
+    't1Full' => 'Tason 1 Tukija',
+    't2Full' => 'Tason 2 Tukija',
+    't1' => 'Taso 2',
+    't2' => 'Taso 2',
+
+    //description of supporter rewards
+    'nameIcon' => 'Nimi kuvake',
+    'nameIconDesc' => 'Valitse yksi 10 kuvakkeesta (mukaan lukien maasi lippu), joka näkyy nimesi edessä.',
+    'emotes' => 'Hymiöt chatissa',
+    'emotesDesc' => 'Kun kirjoitat <code>:</code> jstris-chatissa, hymiövalinta aukeaa yli 2000 Openmoji-kirjastosta saatavilla olevilla hymiöllä.',
+    'discord' => 'Rooli Jstris discordissa',
+    'discordDesc' => 'Oranssi nimi Jstris Discord -palvelimella. Koskee vain, jos linkität Discord-tilin Jstrisiin.',
+    'list' => 'Nimi Tukijat -listalla',
+    'listDesc' => 'Linkki pelin sisäiseen profiiliisi sisällytetään kannattajaluetteloon. Voidaan poistaa asetuksista.',
+
+    'customColor' => 'Mukautettu väri chatissa',
+    'customColorDesc' => 'Voit määrittää minkä tahansa haluamasi nimen värin käyttämällä värinvalitsintyökalua.',
+    'moreIcons' => '2000+ nimi kuvaketta',
+    'moreIconsDesc' => 'Voit asettaa minkä tahansa Openmoji-kirjaston yli 2000 emoji-kuvakkeesta nimeäsi edeltäväksi kuvakkeeksi pelissä ja verkkosivustolla. Voit myös ottaa satunnaisen kuvakkeen valinnan käyttöön joka kerta, kun lataat pelin.',
+    'blockSkins' => '100+ enemmän palikkaskinejä',
+    'blockSkinsDesc' => '100+ enemmän palikkaskiniä ovat saatavilla pelin ulkoasuasetuksissa. Tämä skin-valinta näkyy muille pelaajille ja myös toistoissasi.',
+    'changeName' => 'Mahdollisuus vaihtaa käyttäjätunnusta.',
+    'changeNameDesc' => 'Mahdollisuus vaihtaa käyttäjätunnusta kerran kuukaudessa.',
+    'allFromT1' => 'ja kaikki tasosta 1',
+
+    'perMonth' => ':cost kuukaudessa', //":cost" is replaced with number
+    'customAmount' => 'Mukautettu määrä',
+    'customAmountDesc' => 'Sinusta voi tulla tukija kertaluonteisella maksutapahtumalla tai mukautetulla summalla. Kertaluonteisessa tapauksessa tukijan edut määrätään tilillesi summan perusteella määräajaksi.',
+    'oneTime' => 'Kerran', //max. 8chars
+    'monthly' => 'Kuukausi',  //max. 8chars
+    'other' => 'Muu',  // meaning Other amount than suggested
+    'estimate' => 'Näytä arvio',
+    'dAmount' => 'Lahjoituksen määrä',
+    'sDuration' => 'Tukijan kesto',
+    'learnMore' => 'Lue lisää', //show more information
+    'paymentsInfo' => 'Käsitelty käsittelijän :service_name&nbsp; kautta. Voidaan peruuttaa milloin tahansa tilin kojelaudassa.',
+
+    'gift' => 'Lahjapalkinnot',
+    'giftDesc' => '', // Unused
+    'giftTo' => 'Lahja käyttäjälle',
+    'set' => 'Hyväksy', //confirm, ok, apply,...
+
+    'rewardsAvailable' => 'Tukijan palkinnot saatavilla',
+    'rewardsList' => 'Kiitos lahjoituksestasi Jstriskselle! Voit ottaa käyttöön seuraavat palkinnot.',
+    'monthlyActive' => 'Sinulla on tällä hetkellä :type subscription active.',
+    'customActive' => 'Sinulla on :type rewards enabled for :days päivää lahjoituksen perusteella',
+
+    //configuration
+    'gift' => 'Lahjapalkinnot',
+    'ingameIcon' => 'Pelinsisäinen kuvake',
+    'countryFlag' => 'Maan lippu',
+    'iconEmote' => 'Mukautettu hymiö pelin sisäisenä kuvakkeena',
+    'iconEmoteDesc' => 'Aloita kirjoittaminen etsiäksesi. Vieraile :link nähdäksesi täydellisen luettelon emote-nimistä.',
+    'randIcon' => 'Valitse uusi kuvake satunnaisesti aina, kun peli ladataan',
+    'chatColor' => 'Mukautettu pelin sisäisen nimen väri',
+    'supporterList' => 'Käyttäjänimi mainittu kannattajaluettelossa',
+    'roleAdded' => 'Lisätty yhdistetylle Discord-tilillesi :name.',
+    'addDiscord' => 'Lisää Discord tilisi <a href=":link">Jstris-tilin asetuksiin</a> ennen käyttöönottoa.',
+    'apply' => 'Ota muutokset käyttöön',
+];
